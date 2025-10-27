1150e22a61e22c46eb8c48cf5c8b636cae13445a
Ayush Verma <112403149+AyushVe99@users.noreply.github.com>
2025-10-27 13:01:16 +0530

    Time: 39 ms (5.24%) | Memory: 40.9 MB (56.08%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+
+        for (long i = 1; i * i <= x; i++) {
+            if (i * i == x) {
+                return (int)i;
+            } else if ((i + 1) * (i + 1) > x) {
+                return (int)i;
+            }
+        }
+        return 0; 
+    }
+}
